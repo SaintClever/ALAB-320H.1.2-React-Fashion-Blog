156bfab0cd42fa6ab60e5bb02cd1717ce5a1ea37
Saint. Clever <parchment@gmx.com>
2024-12-07 15:11:29 -0500

    Update UI

--- fashionBlog/src/App.jsx
+++ fashionBlog/src/App.jsx
@@ -5,9 +5,27 @@ function App() {
   return (
     <>
       <div>
-        <nav>
-          Hello
+        <heade>
+          <h1>Sartre's List</h1>
+          <h2>Better-Dressed People</h2>
+        </heade>
+
+        <nav style={{ display: "flex", textDecoration: "none", color: "grey" }}>
+          <a href="#">Women's</a>
+          <a href="#">Men's</a> 
+          <a href="#">On the Street</a> 
+          <a href="#">The Catwalk</a> 
+          <a href="#">AdWatch</a> 
+          <a href="#">About</a> 
         </nav>
+
+        <div>
+          11/12/20
+          <h2>One The Street in Brooklyn</h2>
+          <img src="./assets/image1.jpg" />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa necessitatibus fugit quos quia! Quis, perspiciatis repellendus. Quam dolorem quibusdam assumenda officiis, exercitationem, voluptatibus sit, quas ad praesentium natus voluptatem cumque?</p>
+          <p >continue...</p>
+        </div>
       </div>
     </>
   )

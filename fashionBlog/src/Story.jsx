9de1965b7d820e5491776a2079583bb65fd8a69a
Saint. Clever <parchment@gmx.com>
2024-12-08 07:31:57 -0500

    Update layout

--- fashionBlog/src/Story.jsx
+++ fashionBlog/src/Story.jsx
@@ -1,12 +1,11 @@
 import { data } from "./Data.jsx";
 
 let Story = () => {
-
   const story = data.map(article => 
     <div key={article.id}>
     <p>{article.date}</p>
     <h2>{article.title}</h2>
-    <img src={article.image} />
+    <img src={article.image}/>
     <p>{article.summary}</p>
     <p style={{ textAlign: "right", color: "red"}}>continue...</p>
     </div>
